df8540edb55e6edd040d6ed29087cc487a35bfd8
aunsuwijak <ppnk2537@gmail.com>
2014-06-21 16:24:28 +0700

    add avatar and resume column in users table

--- db/migrate/20140621091812_create_users.rb
+++ db/migrate/20140621091812_create_users.rb
@@ -2,7 +2,10 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-
+      #mark avatar and resume as attachments, and paperclip
+      #will create the proper columns in the users table on migration
+      t.attachment :avatar
+      t.attachment :resume
       t.timestamps
     end
   end
