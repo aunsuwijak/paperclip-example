9da5ed97edd1506348c7902a4513633333f87091
aunsuwijak <ppnk2537@gmail.com>
2014-06-21 16:27:30 +0700

    specify paperclip attachment in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  #specify that the avatar is a paperclip file attachment
+  #specify additional styles that you want to use in views or eslewhere
+  has_attached_file :avatar, :styles => {:thumb => "100x100>"}
+  
+  #specify that the resume is a paperclip file attachment
+  has_attached_file :resume
 end
