3a76add12d644603bf8f215c8ed1d2566ceafbc0
aunsuwijak <ppnk2537@gmail.com>
2014-06-21 16:55:40 +0700

    add validation to user model. edit user permit in user controller. edit view to show avatar and filename.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   #specify that the avatar is a paperclip file attachment
   #specify additional styles that you want to use in views or eslewhere
   has_attached_file :avatar, :styles => {:thumb => "100x100>"}
+  validates_attachment :avatar, :content_type => { :content_type => "image/jpeg" }
   
   #specify that the resume is a paperclip file attachment
   has_attached_file :resume
+  validates_attachment :resume, :content_type => { :content_type => "application/msword"}
 end
